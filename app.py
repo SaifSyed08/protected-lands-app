91c8469562445e410ac44612495f45a20648418c
SaifSyed08 <saifsyed2999@gmail.com>
2025-11-01 18:21:18 -0500

    Fix Earth Engine initialization check

--- app.py
+++ app.py
@@ -12,15 +12,14 @@ import pydeck as pdk
 import ee
 
 # Initialize Earth Engine only if not already initialized
-if not ee.data.isInitialized():
-    service_account = st.secrets.get("GEE_SERVICE_ACCOUNT")
-    key_dict = json.loads(st.secrets.get("GEE_PRIVATE_KEY"))
+if not ee.isInitialized():
+    service_account = st.secrets["GEE_SERVICE_ACCOUNT"]
+    key_dict = json.loads(st.secrets["GEE_PRIVATE_KEY"])
     with tempfile.NamedTemporaryFile(mode="w", delete=False, suffix=".json") as tmp_file:
         json.dump(key_dict, tmp_file)
         tmp_file.flush()
         credentials = ee.ServiceAccountCredentials(service_account, tmp_file.name)
         ee.Initialize(credentials)
-
 
 # Page configuration
 st.set_page_config("Find Similar Protected Lands", layout="wide")
